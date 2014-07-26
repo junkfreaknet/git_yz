53a7117687b5858e4c757b58f8fb22f2e07a7cf0
Noriyuki Takarabe <junk.freak.net@gmail.com>
2014-07-27 03:11:15 +0900

    2014/07/27(sun) commit and push

--- yz/src/yz/logistic/print/StandardSheetB4.java
+++ yz/src/yz/logistic/print/StandardSheetB4.java
@@ -2,6 +2,14 @@ package yz.logistic.print;
 
 public class StandardSheetB4 implements java.awt.print.Printable{
 
+	private mycommons.db.SQLString SQL;
+	
+	//constructor
+	public StandardSheetB4(mycommons.db.SQLString in_sql){
+		
+		this.SQL=in_sql.getSQLString();
+		
+	}
 	//print routine
 	public int print(java.awt.Graphics g,java.awt.print.PageFormat pf,int page) throws java.awt.print.PrinterException{
 
