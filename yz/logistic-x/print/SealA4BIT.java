07a4a1a954f98b84e57549541a2943dcf9219e9c
Noriyuki Takarabe <junk.freak.net@gmail.com>
2014-08-11 02:41:45 +0900

    2014/08/11(mon) 02:41 commit and push

--- yz/logistic-x/print/SealA4BIT.java
+++
@@ -1,8 +0,0 @@
-package yz.logistic.print;
-
-public class SealA4BIT implements java.awt.print.Printable {
-
-	public int print(java.awt.Graphics g,java.awt.print.PageFormat pf,int page) throws java.awt.print.PrinterException{
-		return 0;
-	}
-}
