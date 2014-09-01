d2591824877ca7f04cfd0f6b4dbdf74e1a728b40
Noriyuki Takarabe <junk.freak.net@gmail.com>
2014-09-02 04:21:59 +0900

    2014/09/02(tue) 04:21 commit and push

--- yz/src/yz/logistic/print/sheets/PrintSheets.java
+++ yz/src/yz/logistic/print/sheets/PrintSheets.java
@@ -3,7 +3,7 @@ package yz.logistic.print.sheets;
 public class PrintSheets {
 
 	//print sheets by block numbers
-	public static void print(Object drawer,yz.logistic.print.sheets.parameters.ByBlock para){
+	public static void print(Object drawer,mycommons.db.connection.ParaConnection paraConnection,yz.logistic.print.sheets.parameters.ByBlock paraBlock){
 		/***
 		 EXPLANATION
 		 drawer...an object of sheet drawer 
