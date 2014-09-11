06c8eb53c29b918e4c71321b78a40976d899ad21
Noriyuki Takarabe <junk.freak.net@gmail.com>
2014-09-12 04:15:52 +0900

    2014/09/12(fri) 04:15 commit and push

--- yz/src/xml/MainTestXML.java
+++ yz/src/xml/MainTestXML.java
@@ -17,15 +17,21 @@ public class MainTestXML {
 			if(root.getNodeType()==org.w3c.dom.Node.DOCUMENT_NODE){
 				System.out.println("root is document.");
 			}
+			org.w3c.dom.Document document=(org.w3c.dom.Document)root;
+			org.w3c.dom.Element element=document.getDocumentElement();
+			System.out.println("element name is "+element.getNodeName());
 			
 			org.w3c.dom.Node firstLevel=root.getFirstChild();
 			System.out.println("first child is "+firstLevel.getNodeName()+".");
-			
+			org.w3c.dom.Node lastnode=root.getLastChild();
+			System.out.println("last chile is "+lastnode.getNodeName());
 			org.w3c.dom.Node secondLevel=firstLevel.getFirstChild();
 			if(secondLevel==null){
 				System.out.println("can't get second level child.");
+			}else{
+				System.out.println("second child is "+secondLevel.getNodeName()+".");
 			}
-			System.out.println("second child is "+secondLevel.getNodeName()+".");
+			
 			
 		}catch(Exception e){
 			System.out.println("Error:on building document builder ");
