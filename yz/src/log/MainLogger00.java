ea22fbad77c396c8a23489df3f51f51212fe83f6
Noriyuki Takarabe <junk.freak.net@gmail.com>
2014-09-27 02:35:29 +0900

    2014/09/27(sat) 02:35 commit and push

---
+++ yz/src/log/MainLogger00.java
@@ -0,0 +1,13 @@
+package log;
+
+import java.util.logging.Logger;
+
+public class MainLogger00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//java.util.logging.Logger logger=java.util.logging.Logger.getLogger();
+	}
+
+}
